e1850883a17b68744f94f4451cd01d54c907f15c
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2023-09-20 16:26:45 +0300

    Add some cases for the accessibility demo

--- accessibility-checker-for-vaadin-demo/src/main/java/org/vaadin/addons/accessibility/InputLabelExistsView.java
+++ accessibility-checker-for-vaadin-demo/src/main/java/org/vaadin/addons/accessibility/InputLabelExistsView.java
@@ -1,15 +1,16 @@
 package org.vaadin.addons.accessibility;
 
+import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@PageTitle("test")
-@Route("")
-public class View extends VerticalLayout {
+@PageTitle("input_label_exists")
+@Route("input_label_exists")
+public class InputLabelExistsView extends Main {
 
-    public View() {
+    public InputLabelExistsView() {
         TextField textField = new TextField();
         add(textField);
     }
--- accessibility-checker-for-vaadin-demo/src/main/java/org/vaadin/addons/accessibility/InputLabelVisibleView.java
+++ accessibility-checker-for-vaadin-demo/src/main/java/org/vaadin/addons/accessibility/InputLabelVisibleView.java
@@ -1,16 +1,17 @@
 package org.vaadin.addons.accessibility;
 
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@PageTitle("test")
-@Route("")
-public class View extends VerticalLayout {
+@PageTitle("input_label_visible")
+@Route("input_label_visible")
+public class InputLabelVisibleView extends Main {
 
-    public View() {
+    public InputLabelVisibleView() {
         TextField textField = new TextField();
+        textField.setAriaLabel("Invisible label");
         add(textField);
     }
 }
